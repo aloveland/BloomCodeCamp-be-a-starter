3d403132159db2d5f46979bc586ac13d0f4cd2fe
Alec Loveland <ajlovelandwater@gmail.com>
2023-10-28 00:58:28 -0400

    trying to get tomcat running

--- src/main/java/com/hcc/BackendApplication.java
+++ src/main/java/com/hcc/BackendApplication.java
@@ -1,13 +1,27 @@
 package com.hcc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		log.info("Server is set to run on port: " + serverPort);
+	}
 }
